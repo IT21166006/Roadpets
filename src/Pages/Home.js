96636fb89ac487f7a4d62f398700047cfed926e0
Tdmaxer <ytdilshan07@gmail.com>
2025-02-26 19:06:21 +0530

    Add Home.js

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+// src/pages/Home.js
+import React from 'react';
+import PostForm from '../components/PostForm';
+import PostList from '../components/PostList';
+
+const Home = () => {
+    return (
+        <div>
+           
+            
+            <PostList />
+        </div>
+    );
+};
+
+export default Home;
