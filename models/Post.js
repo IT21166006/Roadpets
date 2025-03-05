aaec6a364bf76743279b97299c4bb14a9fba77e4
Tdmaxer <ytdilshan07@gmail.com>
2025-03-05 19:14:29 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,28 @@
 import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    location: { type: String, required: true },
-    description: { type: String, required: true },
-    images: [{ type: String, required: true }],
-    createdAt: { type: Date, default: Date.now },
-    
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    images: [{
+        type: String
+    }],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
 });
 
-
-const Post = mongoose.model('Post', postSchema);
-
-export default Post;
+export default mongoose.model('Post', postSchema);
