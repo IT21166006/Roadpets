ed8f6b820d41b918389e59cbb2afa0bc55a9dcc4
Tdmaxer <ytdilshan07@gmail.com>
2025-03-12 17:38:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Logo from "../asserts/logo.png";
 import '../CSS/navbar.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const isAuthenticated = localStorage.getItem('token') !== null;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={{ paddingLeft: '30px' }}></div>
@@ -22,45 +25,45 @@ const Navbar = () => {
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto px-5">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item active">
-            <a className="nav-link nav-link-large" href="/home">Home <span className="visually-hidden">(current)</span></a>
+            <a className="nav-link nav-link-large" href="/">Home <span className="visually-hidden">(current)</span></a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link nav-link-large" href="/create">Create Post</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link nav-link-large" href="/create">Projects</a>
-          </li>
+          
+          {/* Only show Create Post if user is authenticated */}
+          {isAuthenticated && (
+            <li className="nav-item">
+              <a className="nav-link nav-link-large" href="/create">Create Post</a>
+            </li>
+          )}
+          
           <li className="nav-item">
             <a className="nav-link nav-link-large" href="/gallery">Gallery</a>
           </li>
-          <li className="nav-item px-3">
-            <a 
-              className="btn-signup" 
-              href="/signup"
-            >
-              SignUp 
-            </a>
-          </li>
           
-          <li className="nav-item px-3">
-            <a 
-              className="btn-signup me-2" 
-              href="/login"
-            >
-              Login
-            </a>
-            
-          </li>
-          <li className="nav-item px-3">
-            <a href="/profile"><AccountCircleIcon style={{ color: 'black', fontSize: '50px', cursor: 'pointer' }} /></a>
+          {/* Show Sign Up and Login only if user is not authenticated */}
+          {!isAuthenticated ? (
+            <>
+              <li className="nav-item px-3">
+                <a className="btn-signup" href="/signup">
+                  SignUp 
+                </a>
+              </li>
+              <li className="nav-item px-3">
+                <a className="btn-signup me-2" href="/login">
+                  Login
+                </a>
+              </li>
+            </>
+          ) : (
+            <li className="nav-item px-3">
+              <a href="/profile">
+                <AccountCircleIcon style={{ color: 'black', fontSize: '50px', cursor: 'pointer' }} />
+              </a>
             </li>
-          
+          )}
         </ul>
-        
       </div>
-      
     </nav>
   );
 };
