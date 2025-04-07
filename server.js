4158b0963d19b3e09e1460cd143233a82610c282
Tdmaxer <ytdilshan07@gmail.com>
2025-04-07 09:02:03 +0530

    origin

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ const __dirname = dirname(__filename);
 
 // CORS Configuration with more specific settings
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['https://roadpets.onrender.com'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
     credentials: true
 }));
 
@@ -66,7 +67,7 @@ app.use('/api/posts', postRoutes);
 // Add this to your existing middleware setup
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
